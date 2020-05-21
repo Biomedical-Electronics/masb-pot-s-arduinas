e50a03eb94294a0a0263ca6c95f92becb1bdbbe8
JuliaReyVilches <juliareyvilches@gmail.com>
2020-05-21 17:03:02 +0200

    Initial commit

---
+++ Core/Inc/components/adc.h
@@ -0,0 +1,15 @@
+/*
+ * adc.h
+ *
+ *  Created on: 21 de maig 2020
+ *      Author: julia
+ */
+
+#ifndef INC_COMPONENTS_ADC_H_
+#define INC_COMPONENTS_ADC_H_
+
+uint32_t voltageADC= 0;
+uint32_t currentADC= 0;
+
+
+#endif /* INC_COMPONENTS_ADC_H_ */
