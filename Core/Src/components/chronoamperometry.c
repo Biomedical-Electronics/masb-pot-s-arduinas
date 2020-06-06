f32da989b59844687dd4954d537b5be6bbae8318
claudia-aymerich <claudiaaymerich@gmail.com>
2020-06-06 14:33:21 +0200

    Cambios en algoritmo CA y CV

--- Core/Src/components/chronoamperometry.c
+++ Core/Src/components/chronoamperometry.c
@@ -19,7 +19,7 @@ struct CA_Configuration_S caConfiguration;
 
 void Chronoamperometry(struct CA_Configuration_S caConfiguration){ //funcion que realiza la cronoamperometria
 
-	TotalMeasuresNumber=(caConfiguration.measurementTime)*10e3/caConfiguration.samplingPeriodMs;
+	TotalMeasuresNumber=((caConfiguration.measurementTime)*1000)/caConfiguration.samplingPeriodMs;
 	//calculamos el total de medidas que debemos realizar
 
 	double voltageDAC=1.65-caConfiguration.eDC/2; //definimos la tension de la celda (Vcell) a partir de tension eDC
@@ -39,14 +39,14 @@ void Chronoamperometry(struct CA_Configuration_S caConfiguration){ //funcion que
 		while(wait == TRUE) { //cuando transcurre el periodo entre medidas
 
 			struct Data_S data;
-			measureIndex++;
 
 			data.point=measureIndex; //guardamos el numero de medida en el que estamos
 			data.timeMs = measureIndex*caConfiguration.samplingPeriodMs; //guardamos el tiempo de medida transcurrido
 			data.voltage = (1.65-GetVoltageADC())*2; //medimos Vcell(real) con los datos del ADC
-			data.current = (GetCurrentADC()-1.65)*2/10e3; //medimos Icell con los datos del ADC
+			data.current = (GetCurrentADC()-1.65)*2/1000; //medimos Icell con los datos del ADC
 
 			MASB_COMM_S_sendData(data); //enviamos datos al host (aplicacion Visense)
+			measureIndex++;
 
 			wait = FALSE; //desactivamos bandera para esperar hasta que pase el periodo entre medidas
 		}
--- Core/Src/components/cyclic_voltammetry.c
+++ Core/Src/components/cyclic_voltammetry.c
@@ -20,12 +20,13 @@ struct CV_Configuration_S cvConfiguration;
 void CyclicVoltammetry(struct CV_Configuration_S cvConfiguration){ //funcion que realiza la voltametria ciclica
 
 	cycle=0; //inicializamos el numero de ciclos
+	VCELL = cvConfiguration.eBegin;
 
-	double VCELL= 1.65-cvConfiguration.eBegin/2; //definimos la tension de la celda (Vcell) a partir de tension eBegin
+	double voltageDACCV= 1.65-VCELL/2; //definimos la tension de la celda (Vcell) a partir de tension eBegin
 
 	vObjetivo = cvConfiguration.eVertex1; //fijamos el valor de vObjetivo a tension eVertex1
 
-	//MASB_COMM_i2c_sendData(VCELL); //enviamos la tension al DAC para fijarla en la celda mediante i2c
+	//MASB_COMM_i2c_sendData(voltageDACCV); //enviamos la tension al DAC para fijarla en la celda mediante i2c
 
 	HAL_GPIO_WritePin(RELAY_GPIO_Port, RELAY_Pin, GPIO_PIN_SET); //cerramos rele, empieza la medida
 
@@ -39,55 +40,61 @@ void CyclicVoltammetry(struct CV_Configuration_S cvConfiguration){ //funcion que
 			while(wait) { //cuando transcurre el periodo entre medidas
 
 				struct Data_S data;
-				measureIndexCV++;
 
 				data.point=measureIndexCV; //guardamos el numero de medida en el que estamos
 				data.timeMs = measureIndexCV*(cvConfiguration.eStep/cvConfiguration.scanRate); //guardamos el tiempo de medida transcurrido
 				data.voltage = (1.65-GetVoltageADC())*2; //medimos Vcell(real) con los datos del ADC
-				data.current = (GetCurrentADC()-1.65)*2/10e3; //medimos Icell con los datos del ADC
+				data.current = (GetCurrentADC()-1.65)*2/1000; //medimos Icell con los datos del ADC
 
 				MASB_COMM_S_sendData(data); //enviamos datos al host ( aplicacion Visense)
+				measureIndexCV++;
 
 				if (VCELL==vObjetivo){ //Si Vcell es igual a la tension objetivo definida
 
 						if (vObjetivo==cvConfiguration.eVertex1) { //si el objectivo era eVertex1
 							vObjetivo = cvConfiguration.eVertex2; //pasa a ser eVertex2
 
+						} else if (vObjetivo==cvConfiguration.eVertex2)  { //si el objectivo era eVertex2
+							vObjetivo = cvConfiguration.eBegin; //pasa a ser eBegin
+
+						} else if (vObjetivo==cvConfiguration.eBegin) { //si el objectivo era eBegin
+							vObjetivo = cvConfiguration.eVertex1; //definimos el objectivo como eVertex1
+							cycle = cycle +1 ; //empezamos nuevo ciclo
+						}
+
+				} else { //Si, en cambio, Vcell no es igual a la tension objetivo definida
+					if (vObjetivo == cvConfiguration.eVertex1 ){ //si el objetivo es eVertex1
+						if (VCELL + cvConfiguration.eStep > vObjetivo){ //si Vcell + el step es superior al objetivo
+							VCELL = vObjetivo;
+							voltageDACCV= 1.65-VCELL/2; //definimos la tension de la celda (Vcell) a la tensio objetivo
+							//MASB_COMM_i2c_sendData(voltageDACCV); //enviamos la tension al DAC para fijarla en la celda mediante i2c
 						} else {
-							if (vObjetivo==cvConfiguration.eVertex2){ //si el objectivo era eVertex2
-								vObjetivo = cvConfiguration.eBegin; //pasa a ser eBegin
-								cycle++; //empezamos nuevo ciclo
-
-							} else { //si no era eVertex2
-								if (cycle == cvConfiguration.cycles){ //y si es el ultimo ciclo
-									HAL_GPIO_WritePin(RELAY_GPIO_Port, RELAY_Pin, GPIO_PIN_RESET); //abrimos rele, se acaba la medida
-									HAL_TIM_Base_Stop_IT(&htim3); //paramos el timer
-
-								} else{ //y tampoco no es el ultimo ciclo
-									vObjetivo = cvConfiguration.eVertex1; //definimos el objectivo como eVertex1
-								}
-							}
-
+							VCELL = VCELL + cvConfiguration.eStep; //le sumamos eStep a Vcell
 						}
-
-						} else { //Si, en cambio, Vcell no es igual a la tension objetivo definida
-							if (VCELL + cvConfiguration.eStep > vObjetivo){ //si Vcell + el step es superior al objetivo
-								VCELL= 1.65-vObjetivo/2; //definimos la tension de la celda (Vcell) a la tensio objetivo
-								//MASB_COMM_i2c_sendData(VCELL); //enviamos la tension al DAC para fijarla en la celda mediante i2c
-
-							} else { // y Vcell + el step no es superior al objetivo
-								if (vObjetivo == cvConfiguration.eVertex1){ //si el objetivo es eVertex1
-									VCELL = VCELL + cvConfiguration.eStep; //le sumamos eStep a Vcell
-								}
-								if (vObjetivo == cvConfiguration.eVertex2){ //si el objetivo es eVertex2
-									VCELL = VCELL - cvConfiguration.eStep;  //le restamos eStep a Vcell
-								}
-								if (vObjetivo == cvConfiguration.eBegin){ //si el objetivo es eBegin
-									VCELL = VCELL + cvConfiguration.eStep; //le sumamos eStep a Vcell
-								}
-							}
-							wait = FALSE; //desactivamos bandera para esperar hasta que pase el periodo entre medidas
+					}
+					if (vObjetivo == cvConfiguration.eVertex2){ //si el objetivo es eVertex2
+						if (VCELL - cvConfiguration.eStep < vObjetivo){ //si Vcell - el step es inferior al objetivo
+							VCELL = vObjetivo;
+							voltageDACCV= 1.65-VCELL/2; //definimos la tension de la celda (Vcell) a la tensio objetivo
+							//MASB_COMM_i2c_sendData(voltageDACCV); //enviamos la tension al DAC para fijarla en la celda mediante i2c
+						} else {
+							VCELL = VCELL - cvConfiguration.eStep;  //le restamos eStep a Vcell
 						}
+					}
+					if (vObjetivo == cvConfiguration.eBegin ){ //si el objetivo es eBegin
+						if (VCELL + cvConfiguration.eStep > vObjetivo){ //si Vcell + el step es superior al objetivo
+							VCELL = vObjetivo;
+							voltageDACCV= 1.65-VCELL/2; //definimos la tension de la celda (Vcell) a la tensio objetivo
+							//MASB_COMM_i2c_sendData(voltageDACCV); //enviamos la tension al DAC para fijarla en la celda mediante i2c
+						} else {
+							VCELL = VCELL + cvConfiguration.eStep; //le sumamos eStep a Vcell
+						}
+					}
+				}
+				wait = FALSE; //desactivamos bandera para esperar hasta que pase el periodo entre medidas
+			}
 		}
+		HAL_GPIO_WritePin(RELAY_GPIO_Port, RELAY_Pin, GPIO_PIN_RESET); //abrimos rele, se acaba la medida
+		HAL_TIM_Base_Stop_IT(&htim3); //paramos el timer
 	}
-}
+
